0f4f59d021865d404d71fa76b4c048b794e88d05
timkofu <509078+timkofu@users.noreply.github.com>
2020-01-28 15:27:15 +0400

    commencement

---
+++ coursework_1/code.py
@@ -0,0 +1,27 @@
+
+# F21BD Coursework 1 Source code
+
+
+class Task:
+    """ Tasks base class, with shared state and behavior """
+
+    __slots__ = ()
+
+    def __init__(self):
+        pass
+
+
+class One(Task):
+    __slots__ = ()
+
+
+class Two(Task):
+    __slots__ = ()
+
+
+class Three(Task):
+    __slots__ = ()
+
+
+if __name__ == "__main__":
+    pass
