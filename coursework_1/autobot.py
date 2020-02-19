82ddebfb22d79cc1ccbe2132d1bde32caf5ba9ab
timkofu <509078+timkofu@users.noreply.github.com>
2020-02-19 14:09:44 +0400

    remodelling:done, beginning code write, and cypher re-writes

--- coursework_1/autobot.py
+++ coursework_1/autobot.py
@@ -1,7 +1,62 @@
 
 # F21BD Coursework 1 Source code
 
+import
 
+import pandas
+
+
+# Data Cleanup
+class DataCleanup:
+
+    __slots__ = ()
+
+    def __init__(self):
+        pass
+
+
+class Ratings(DataCleanup):
+
+    __slots__ = ()
+
+
+class MoviesToWriters(DataCleanup):
+
+    __slots__ = ()
+
+
+class MoviesToActors(DataCleanup):
+
+    __slots__ = ()
+
+
+class Movies(DataCleanup):
+
+    __slots__ = ()
+
+
+class Directors(DataCleanup):
+
+    __slots__ = ()
+
+
+class Writers(DataCleanup):
+
+    __slots__ = ()
+
+
+class Actors(DataCleanup):
+
+    __slots__ = ()
+
+
+class RunningTimes(DataCleanup):
+
+    __slots__ = ()
+
+
+
+# Tasks
 class Task:
     """ Tasks base class, with shared state and behavior """
 
@@ -12,14 +67,17 @@ class Task:
 
 
 class One(Task):
+
     __slots__ = ()
 
 
 class Two(Task):
+
     __slots__ = ()
 
 
 class Three(Task):
+
     __slots__ = ()
 
 
